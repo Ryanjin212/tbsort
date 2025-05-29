072f8477cada7b2b4f6d26aa695b82963ca3e792
Ryanjin212 <155362295+Ryanjin212@users.noreply.github.com>
2025-05-29 19:56:51 +0800

    Update madouclub.js

--- js/madouclub.js
+++ js/madouclub.js
@@ -8,7 +8,7 @@ var rule = {
     },
     timeout:5000,
     class_name:'麻豆传媒&麻豆番外篇&麻豆花絮&hongkongdoll&psychoporntw&果冻传媒&天美传媒&皇家华人&兔子先生&点赞排行&7天热门&30天热门',//静态分类名称拼接
-    class_url:'麻豆传媒&麻豆番外篇&麻豆花絮&hongkongdoll#&psychoporntw&果冻传媒&天美传媒&皇家华人&兔子先生&likes&week&month',//静态分类标识拼接
+    class_url:'麻豆传媒&麻豆番外篇&麻豆花絮&hongkongdoll&psychoporntw&果冻传媒&天美传媒&皇家华人&兔子先生&likes&week&month',//静态分类标识拼接
     limit:5,
     play_parse:true,
     lazy:'',
