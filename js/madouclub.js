6bcdb14a9165bee5624444e7ab1d172232e29d8d
Ryanjin212 <155362295+Ryanjin212@users.noreply.github.com>
2025-05-29 15:11:47 +0800

    Update madouclub.js

--- js/madouclub.js
+++ js/madouclub.js
@@ -8,11 +8,11 @@ var rule = {
     },
     timeout:5000,
     class_name:'麻豆传媒&麻豆番外篇&麻豆花絮&hongkongdoll&psychoporntw&果冻传媒&天美传媒&皇家华人&兔子先生',//静态分类名称拼接
-    class_url:'%e9%ba%bb%e8%b1%86%e4%bc%a0%e5%aa%92;&%e9%ba%bb%e8%b1%86%e7%95%aa%e5%a4%96%e7%af%87;&%e9%ba%bb%e8%b1%86%e8%8a%b1%e7%b5%ae;&hongkongdoll&psychoporntw&果冻传媒&天美传媒&皇家华人&兔子先生',//静态分类标识拼接
+    class_url:'麻豆传媒&麻豆番外篇&麻豆花絮&hongkongdoll&psychoporntw&果冻传媒&天美传媒&皇家华人&兔子先生',//静态分类标识拼接
     limit:5,
     play_parse:true,
     lazy:'',
-    一级:'.excerpts div&&a;h2&&Text;.excerpt.excerpt-c5&&data-src;h2&&Text;a&&href',
+    一级:'.excerpts div&&a;h2&&Text;img&&data-src;h2&&Text;a&&href',
     二级:'*',
 	搜索:'.grid.grid-cols-2 div&&a;h2&&Text;.lozad.w-full&&data-src;h2&&Text;a&&href',
 	searchable:1,//是否启用全局搜索,
