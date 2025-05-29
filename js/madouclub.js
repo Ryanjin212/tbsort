7d44d6b93f652473b7e9f79ddd296d03c7e8979f
Ryanjin212 <155362295+Ryanjin212@users.noreply.github.com>
2025-05-29 16:41:34 +0800

    Update madouclub.js

--- js/madouclub.js
+++ js/madouclub.js
@@ -13,7 +13,7 @@ var rule = {
     limit:5,
     play_parse:true,
     lazy:'',
-    一级:'.excerpts-wrapper&&a;h2&&Text;img&&data-src;.absolute.bottom-1&&Text;a&&href',
+    一级:'.excerpts-wrapper&&div;h2&&Text;img&&data-src;.absolute.bottom-1&&Text;a&&href',
     二级:'*',
 	搜索:'.grid.grid-cols-2 div&&a;h2&&Text;.lozad.w-full&&data-src;.absolute.bottom-1&&Text;a&&href',
 	searchable:1,//是否启用全局搜索,
