46ccd0c8a4313fc8b95655d1a5b42ed30ecfa33a
Ryanjin212 <155362295+Ryanjin212@users.noreply.github.com>
2025-05-29 11:29:36 +0800

    Create madouclub.js

---
+++ js/madouclub.js
@@ -0,0 +1,21 @@
+var rule = {
+    title:'Madou.club',
+    host:'https://madou.club',
+	searchUrl:'/?s=**',
+    url:'/category/fyclass/page/fypage',
+    headers:{
+        'User-Agent':'MOBILE_UA'
+    },
+    timeout:5000,
+    class_name:'麻豆传媒&麻豆番外篇&麻豆花絮&hongkongdoll&91制片厂&果冻传媒&天美传媒&皇家华人&兔子先生&麻豆导演系列&大象传媒&猫爪影像',//静态分类名称拼接
+    class_url:'麻豆传媒&麻豆番外篇&麻豆花絮&hongkongdoll&91制片厂&果冻传媒&天美传媒&皇家华人&兔子先生&麻豆导演系列&大象传媒&猫爪影像',//静态分类标识拼接
+    limit:5,
+    play_parse:true,
+    lazy:'',
+    一级:'.grid.grid-cols-2 div&&a;.lozad.w-full&&alt;.lozad.w-full&&data-src;.absolute.bottom-1&&Text;a&&href',
+    二级:'*',
+	搜索:'.grid.grid-cols-2 div&&a;.lozad.w-full&&alt;.lozad.w-full&&data-src;.absolute.bottom-1&&Text;a&&href',
+	searchable:1,//是否启用全局搜索,
+    quickSearch:1,//是否启用快速搜索,
+    filterable:0,//是否启用分类筛选,
+}
