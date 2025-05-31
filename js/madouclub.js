5396f89d2ca8514d5f2c97adad32113132b9343d
Ryanjin212 <155362295+Ryanjin212@users.noreply.github.com>
2025-05-31 14:29:28 +0800

    Update madouclub.js

--- js/madouclub.js
+++ js/madouclub.js
@@ -6,7 +6,7 @@ var rule = {
     headers:{
        'User-Agent':'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/136.0.0.0 Safari/537.36',
 	'referer'：'https://madou.club/'   ,
-	    'origin'：'https://madou.club'   ,
+	    'origin'：'https://madou.club'
     },
     timeout:5000,
     class_name:'麻豆传媒&麻豆番外篇&麻豆花絮&hongkongdoll&psychoporntw&91制片厂&果冻传媒&天美传媒&皇家华人&兔子先生&爱豆&麻豆导演系列&大象传媒&猫爪影像&精东影业&杏吧&乐播传媒&草莓&抖阴&SA国际传媒&起点传媒/性视界传媒&大鸟十八&小鹏奇啪行&女优淫娃培训营&淫欲游戏王&女神羞羞研究所&突袭女优家&情趣K歌房&KISS糖果屋&点赞排行&7天热门&30天热门',//静态分类名称拼接
