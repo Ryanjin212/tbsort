35122e60794ca5ef78cb2594f0a178ab943308ef
Ryanjin212 <155362295+Ryanjin212@users.noreply.github.com>
2025-06-29 13:27:04 +0800

    Update Missav.js

--- js/Missav.js
+++ js/Missav.js
@@ -8,7 +8,7 @@ var rule = {
     },
     timeout:5000,
     class_name:'最近更新&新作上市&今日热门&本週热门&本月热门&无码流出&VR&SIRO&LUXU&GANA&PRESTIGE&S-CUTE&ARA&FC2&heyzo&tokyohot&1pondo&aribbeancom&caribbeancompr&10musume&pacopacomama&gachinco&xxxav&人妻斩&naughty4610&naughty0930&麻豆传媒&韩国直播&中国直播',//静态分类名称拼接
-    class_url:'dm514/cn/new&dm588/cn/release&dm291/cn/today-hot&dm169/cn/weekly-hot&monthly-hot&dm621/cn/uncensored-leak&dm2091/genres/VR&dm23/cn/siro&dm20/cn/luxu&dm17/cn/gana&dm862/cn/maan&dm23/cn/scute&dm19/cn/ara&dm99/cn/fc2&dm319995/cn/heyzo&dm29/cn/tokyohot&dm695579/cn/1pondo&dm1271239/cn/caribbeancom&dm14081/cn/caribbeancompr&dm1117248/cn/10musume&dm370414/cn/pacopacomama&dm135/cn/gachinco&dm29/cn/xxxav&dm24/cn/marriedslash&dm19/cn/naughty4610&dm22/cn/naughty0930&madou&cn/klive&cn/clive',//静态分类标识拼接
+    class_url:'new&release&today-hot&weekly-hot&monthly-hot&dm621/cn/uncensored-leak&dm2091/genres/VR&dm23/cn/siro&dm20/cn/luxu&dm17/cn/gana&dm862/cn/maan&dm23/cn/scute&dm19/cn/ara&dm99/cn/fc2&dm319995/cn/heyzo&dm29/cn/tokyohot&dm695579/cn/1pondo&dm1271239/cn/caribbeancom&dm14081/cn/caribbeancompr&dm1117248/cn/10musume&dm370414/cn/pacopacomama&dm135/cn/gachinco&dm29/cn/xxxav&dm24/cn/marriedslash&dm19/cn/naughty4610&dm22/cn/naughty0930&madou&cn/klive&cn/clive',//静态分类标识拼接
     limit:5,
     play_parse:true,
     lazy:'',
